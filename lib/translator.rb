d426c8080855953708ab831f7d857d919e2eeb05
Shawn Koski <shawnnkoski@gmail.com>
2018-04-11 03:25:23 +0000

    Learning about YAML

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,34 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+#emojis = YAML.load_file('lib/emoticons.yml')
+def load_library(library)
+  yam_hash = YAML.load_file(library)
+  new_hash = {"get_meaning" => {}, "get_emoticon" => {}}
+  yam_hash.each do |name, emojis|
+   new_hash["get_meaning"][emojis[1]] = name
+   new_hash["get_emoticon"][emojis[0]] = emojis[1]
+  end
+ new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoji)
+ emoji_hash = load_library(path)
+  if emoji_hash["get_emoticon"].include?emoji
+    emoji_hash["get_emoticon"][emoji]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoji)
+  emoji_hash = load_library(path)
+  if emoji_hash["get_meaning"].include?emoji
+     emoji_hash["get_meaning"][emoji]
+  else
+    "Sorry, that emoticon was not found"
+  end
+  
 end
+
+load_library("./lib/emoticons.yml")
